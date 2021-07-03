10e28075cf1addef917532ca787c481358aca8cc
Vanio Begic <vanio.begic123@gmail.com>
2021-07-03 19:50:57 +0200

    Remove some classes which were implemented too early

--- src/main/java/home/appointments/ProviderRepository.java
+++
@@ -1,7 +0,0 @@
-package home.appointments;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProviderRepository extends JpaRepository<Provider, Integer> {
-    Provider findByName(String name);
-}
