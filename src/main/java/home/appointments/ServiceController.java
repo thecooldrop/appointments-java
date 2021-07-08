7991fbb3003bc86090f63317ec5be0f7cb2e3b53
Vanio Begic <vanio.begic123@gmail.com>
2021-07-08 19:10:28 +0200

    Refactor ServiceCrud component away

--- src/main/java/home/appointments/ServiceController.java
+++ src/main/java/home/appointments/ServiceController.java
@@ -7,22 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @RestController
 public class ServiceController {
 
     @Autowired
-    private ServiceCrud serviceCrud;
+    private ServiceDao serviceData;
 
     @GetMapping(path = "/services", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Set<ServiceEntity>> getAllServices() {
-        return ResponseEntity.ok(serviceCrud.read());
+        return ResponseEntity.ok(new HashSet<>(serviceData.findAll()));
     }
 
     @GetMapping(path = "/services/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ServiceEntity> getServiceById(@PathVariable Integer id) {
-        return serviceCrud.readById(id)
+        return serviceData.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
