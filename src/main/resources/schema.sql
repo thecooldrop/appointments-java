b25592360e1160819bc226d629425dd0bce8fe88
Vanio Begic <vanio.begic123@gmail.com>
2021-08-28 23:25:39 +0200

    Start implementing incomplete data model

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@
 -- - An appointment can only be booked at locations where provider is
 -- - A provider can not be booked for multiple appointments at same time
 -- Integrity constraints missing:
--- - Right not an appointment can be provided by multiple providers
+-- - Right now an appointment can be provided by multiple providers
 -- - An appointment can span multiple locations, due to being split across multiple providers
 -- -
 
@@ -18,27 +18,41 @@
 -- Naive:
 -- Idea: maybe we can add an nullable foreign key from provider_location_time to appointment
 ----------------------------------------------------------------------------------------------------------------
+CREATE EXTENSION citext;
+CREATE DOMAIN email AS citext
+  CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
 create table provider (
     id serial primary key,
-    name varchar(128) not null
+    first_name varchar(128) not null check(first_name = lower(first_name))
+    last_name varchar(128) not null check(last_name = lower(last_name))
 );
 create table service (
     id serial primary key,
-    name varchar(255) not null,
-    unique(name)
+    service_price_id integer not null,
+    name varchar(255) not null check(name = lower(name)),
+    description text not null,
+    duration interval not null,
+    unique(name),
+    foreign key (service_price_id) references service_price(id),
+);
+create table service_price (
+    id serial primary key,
+    price_km integer not null check(price_km >= 0),
+    price_fening integer not null check(price_fening >= 0)
 );
 create table location (
     id serial primary key,
-    name varchar(255) not null,
+    name varchar(255) not null check(name = lower(name)),
     unique(name)
 );
-
-create table appointment(
+create table appointment (
     id serial primary key,
-    service_id integer not null
+    service_id integer not null,
+    customer_id varchar(255) not null check(customer_id = lower(customer_id)),
+    customer_phone text check (customer_phone like '+3876[1-6][0-9][0-9][0-9][0-9][0-9][0-9][0-9]?'),
+    customer_mail email
 );
-
-create table provider_location_time(
+create table provider_location_time (
     id serial primary key,
     provider_id integer not null,
     location_id integer not null,
