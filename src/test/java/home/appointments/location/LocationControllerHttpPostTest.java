0940259b0883c9f04cd9d777f9476572ebce9c09
Vanio Begic <vanio.begic123@gmail.com>
2021-09-16 19:35:46 +0200

    Remove unused imports

--- src/test/java/home/appointments/location/LocationControllerHttpPostTest.java
+++ src/test/java/home/appointments/location/LocationControllerHttpPostTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 
